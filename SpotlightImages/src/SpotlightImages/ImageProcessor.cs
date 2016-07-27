dfb9bf57cf3a6c5bb68f24be86b34d25cb5424a1
mdraganov <mariyandraganov@gmail.com>
2016-07-27 18:10:08 +0300

    Added landscape and portrait folders segregation.

--- SpotlightImages/src/SpotlightImages/ImageProcessor.cs
+++ SpotlightImages/src/SpotlightImages/ImageProcessor.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.IO;
+using System.Drawing;
 using Microsoft.Extensions.Configuration;
 
 namespace SpotlightImages
@@ -31,7 +32,7 @@ namespace SpotlightImages
                 Console.Write("Invalid input. Please enter y or n: ");
                 input = Console.ReadLine();
             }
-            
+
             // todo: validate input
             if (input == "y" || input == "Y")
             {
@@ -52,7 +53,7 @@ namespace SpotlightImages
                 Console.ReadLine();
                 return;
             }
-            
+
             var copiedImagesCount = 0;
             var existingImagesCount = 0;
             IEnumerable<FileInfo> files = new DirectoryInfo(this.spotlightImagesFolder).EnumerateFiles();
@@ -65,7 +66,18 @@ namespace SpotlightImages
                     continue;
                 }
 
-                this.CopyFile(file.FullName, destinationFolder + "\\" + file.Name + ".jpg");
+                var size = this.GetJpegImageSize(file.FullName);
+
+                // todo: decide what's portrait and what's landscape
+                if (size.Height < size.Width)
+                {
+                    this.CopyFile(file.FullName, destinationFolder + "\\Landscape\\" + file.Name + ".jpg");
+                }
+                else
+                {
+                    this.CopyFile(file.FullName, destinationFolder + "\\Portrait\\" + file.Name + ".jpg");
+                }
+
                 copiedImagesCount++;
             }
 
@@ -118,5 +130,83 @@ namespace SpotlightImages
         {
             config[key] = value;
         }
+
+        public Size GetJpegImageSize(string filename)
+        {
+            FileStream stream = null;
+            BinaryReader rdr = null;
+            try
+            {
+                stream = File.OpenRead(filename);
+                rdr = new BinaryReader(stream);
+                // keep reading packets until we find one that contains Size info
+                for (;;)
+                {
+                    byte code = rdr.ReadByte();
+                    if (code != 0xFF) throw new Exception("Unexpected value in file " + filename);
+                    code = rdr.ReadByte();
+                    switch (code)
+                    {
+                        // filler byte
+                        case 0xFF:
+                            stream.Position--;
+                            break;
+                        // packets without data
+                        case 0xD0:
+                        case 0xD1:
+                        case 0xD2:
+                        case 0xD3:
+                        case 0xD4:
+                        case 0xD5:
+                        case 0xD6:
+                        case 0xD7:
+                        case 0xD8:
+                        case 0xD9:
+                            break;
+                        // packets with size information
+                        case 0xC0:
+                        case 0xC1:
+                        case 0xC2:
+                        case 0xC3:
+                        case 0xC4:
+                        case 0xC5:
+                        case 0xC6:
+                        case 0xC7:
+                        case 0xC8:
+                        case 0xC9:
+                        case 0xCA:
+                        case 0xCB:
+                        case 0xCC:
+                        case 0xCD:
+                        case 0xCE:
+                        case 0xCF:
+                            ReadBEUshort(rdr);
+                            rdr.ReadByte();
+                            ushort h = ReadBEUshort(rdr);
+                            ushort w = ReadBEUshort(rdr);
+                            return new Size(w, h);
+                        // irrelevant variable-length packets
+                        default:
+                            int len = ReadBEUshort(rdr);
+                            stream.Position += len - 2;
+                            break;
+                    }
+                }
+            }
+            finally
+            {
+                if (rdr != null) rdr.Dispose();
+                if (stream != null) stream.Dispose();
+            }
+        }
+
+        private static ushort ReadBEUshort(BinaryReader rdr)
+        {
+            ushort hi = rdr.ReadByte();
+            hi <<= 8;
+            ushort lo = rdr.ReadByte();
+            return (ushort)(hi | lo);
+        }
+
     }
 }
