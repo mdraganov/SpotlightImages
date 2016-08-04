69b5b399660830fe7b51db953620c86c0013b19a
mdraganov <mariyandraganov@gmail.com>
2016-08-04 17:33:49 +0300

    Renamed some stuff.

--- SpotlightImages/src/SpotlightImages/ProgramUI.cs
+++ SpotlightImages/src/SpotlightImages/ProgramUI.cs
@@ -1,17 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace SpotlightImages
 {
-    public class Program
+    public class ProgramUI
     {
         public static void Main(string[] args)
         {
             var spotlightImagesFolder = Environment.GetEnvironmentVariable("LocalAppData") + @"\Packages\Microsoft.Windows.ContentDeliveryManager_cw5n1h2txyewy\LocalState\Assets";
             var defaultDestinationFolder = "C:/Spotlight images";
-
+                        
+            Process.Start("explorer.exe", "C:\\Spotlight images");
             var imageProcessor = new ImageProcessor(spotlightImagesFolder, defaultDestinationFolder);
             imageProcessor.RetrieveImages();
         }
